888e49a33463d342658ea1098467159cfed5decc
Michael Perlowin <82491078+mperlowin@users.noreply.github.com>
2024-04-08 23:01:52 +0000

    changes

---
+++ client/src/features/navigation/Navigation.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+export function Navigation() {
+    return (
+        <div>
+            <h1>Navigation</h1>
+        </div>
+    );
+}
